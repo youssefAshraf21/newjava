e52449d12064ede35cfeaa4ca4321b694e5536e7
youssefAshraf21 <ya614962@gmail.com>
2025-10-04 17:19:05 +0300

    Add complete HTML structure and initial JavaScript functionality

---
+++ ass6/index.js
@@ -0,0 +1,9 @@
+//the first way to disable this code is make it comment
+/*console.log("Iam In Console");
+document.write("Iam In Page");
+*/
+//the second way to disable this code is make return 
+return;
+
+console.log("Iam In Console");
+document.write("Iam In Page");

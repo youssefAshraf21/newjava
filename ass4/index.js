300514c8a430743f5c2884e1f9303e704c9cd776
youssefAshraf21 <ya614962@gmail.com>
2025-10-04 16:28:02 +0300

    Add HTML structure and JavaScript logging functionality for groups

---
+++ ass4/index.js
@@ -0,0 +1,17 @@
+console.group("group 1");
+console.log("Message One");
+console.log("Message Two");
+console.group("Child Group");
+console.log("Message One");
+console.log("Message Two");
+console.group("Grand Child Group");
+console.log("Message One");
+console.log("Message Two");
+console.groupEnd();
+console.groupEnd();
+console.groupEnd();
+console.group("group 2");
+console.log("Message One");
+console.log("Message Two");
+console.groupEnd();
+

7ec6aaee74a4e4de880f1213d865e54a63f704a3
youssefAshraf21 <ya614962@gmail.com>
2025-10-05 14:16:15 +0300

    Add complete HTML structure and JavaScript solutions for calculations

---
+++ ass11/index.js
@@ -0,0 +1,19 @@
+let num = 3;
+
+// Solution One
+console.log(num * (num-1)); // 6
+
+// Solution Two
+console.log(num + num); // 6
+
+// Soultion Three
+console.log(num * 2); // 6
+
+// Soultion Four
+console.log(num + true + true + true); // 6
+
+// Solution Five
+console.log(num * (num / 1.5)); // 6
+
+// Solution Six
+console.log((num ** 2) / 1.5); // 6

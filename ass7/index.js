f07c7d80f543cce1bda0d79d3e20d4ebbf6cedbd
youssefAshraf21 <ya614962@gmail.com>
2025-10-05 12:59:53 +0300

    Add HTML structure and JavaScript logging functionality for element content

--- ass7/index.js
+++ ass7/index.js
@@ -2,3 +2,9 @@ let num1 = 10;
 let num2 = 20;
 console.log(num1+""+num2);
 console.log(typeof(num1+""+num2));
+console.log(`${num1}${num2}`);
+console.log(typeof(`${num1}${num2}`));
+console.log(`${num1}
+${num2}`);
+console.log(`${num2}
+${num1}`);

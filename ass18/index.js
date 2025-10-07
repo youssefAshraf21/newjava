07d0b1ffdcd3ab7cc5b0480dd7428cf13d9fda32
youssefAshraf21 <ya614962@gmail.com>
2025-10-07 18:52:36 +0300

    Add initial HTML and JavaScript files for number formatting test cases

---
+++ ass18/index.js
@@ -0,0 +1,15 @@
+// Test Case 1
+let num1 = 9; // "009"
+if(num1 < 10){
+    console.log("00" + num1);
+}
+// Test Case 2
+let num2 = 20; // "020"
+if(num2 > 10 && num2 < 100){
+    console.log("0"+ num2);
+}
+// Test Case 3
+let num3 = 110; // "110"
+if(num3 >= 100){
+    console.log(num3);
+}

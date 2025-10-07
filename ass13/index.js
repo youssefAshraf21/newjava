2849671e6e1f925ce6629e5a9aa2297d4801cf0c
youssefAshraf21 <ya614962@gmail.com>
2025-10-07 15:09:12 +0300

    Add HTML structure and JavaScript code for user name manipulation

---
+++ ass13/index.js
@@ -0,0 +1,6 @@
+let userName = "Elzero";
+console.log( userName[0].toLowerCase() ); // e
+console.log(userName.toLowerCase().slice(0,1)); // e
+console.log(userName.charAt(0).toLowerCase()); // e
+console.log(userName.substring(0, 1).toLowerCase()); // e
+console.log(userName[0].toLowerCase().repeat(3)); // eee

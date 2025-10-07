a354e272703b9e733ea15345b513fcf03712e14b
youssefAshraf21 <ya614962@gmail.com>
2025-10-07 15:59:46 +0300

    Add initial HTML and JavaScript files for basic string manipulation

---
+++ ass14/index.js
@@ -0,0 +1,8 @@
+let word = "Elzero";
+let letterZ = "z";
+let letterE = "e";
+let letterO = "O";
+
+console.log(word.includes("z")); // True
+console.log(word.startsWith("E")); // True
+console.log(word.endsWith("o")); // True

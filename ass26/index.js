d738385aff6ce6528e54c5afcd12422b6c43d41c
youssefAshraf21 <ya614962@gmail.com>
2025-10-12 15:33:06 +0300

    Add initial HTML and JavaScript files for basic search example

---
+++ ass26/index.js
@@ -0,0 +1,9 @@
+let needle = "JS";
+let haystack = ["PHP", "JS", "Python"];
+if(haystack.includes(needle)){
+    console.log("Found");
+}
+if(haystack.indexOf(needle) !== -1){
+    console.log("Found");
+}
+// Write 2 Solutions

92f303509ca51bc866fdbb4db3cb1f6de7433180
youssefAshraf21 <ya614962@gmail.com>
2025-10-14 15:55:46 +0300

    Add initial HTML and JavaScript files for number iteration example

---
+++ ass28/index.js
@@ -0,0 +1,19 @@
+let start = 10;
+let end = 100;
+let exclude = 40;
+for (let i = start; i <= end; i += 10) {
+    if (i === exclude) {
+        continue;
+    }
+    console.log(i);
+}
+// Output
+10
+20
+30
+50
+60
+70
+80
+90
+100

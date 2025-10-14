a5ff145f53abfcb2959d7d8c3f8c53f08e1486d4
youssefAshraf21 <ya614962@gmail.com>
2025-10-14 16:23:59 +0300

    Add initial HTML and JavaScript files for number iteration example

---
+++ ass30/index.js
@@ -0,0 +1,28 @@
+let start = 1;
+let end = 6;
+let breaker = 2;
+for (let i = start; i <= end; i++) {
+    console.log(i);
+    for (let j = breaker; j <= breaker * 2; j += breaker) {
+        console.log("-- " + j);
+    }
+}
+// Output
+// 1
+// -- 2
+// -- 4
+// 2
+// -- 2
+// -- 4
+// 3
+// -- 2
+// -- 4
+// 4
+// -- 2
+// -- 4
+// 5
+// -- 2
+// -- 4
+// 6
+// -- 2
+// -- 4

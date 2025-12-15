75f94d382e3d1d30cc5cafa65668cb4c5d24dc14
youssefAshraf21 <ya614962@gmail.com>
2025-12-15 19:59:04 +0200

    Add product section with heading and description to index.js

--- dom2/index.js
+++ dom2/index.js
@@ -14,6 +14,24 @@ my_element.className = "div1";
 my_element.setAttributeNode(my_atribute);
 my_element.appendChild(my_text);
 document.body.appendChild(my_element);
+//*******************************************
+let my_product = document.createElement("div");
+let my_product_heading = document.createElement("h2");
+let my_product_paragraph = document.createElement("p");
+// add heading and paragraph text
+let my_heading_text = document.createTextNode("This is my product");
+let my_paragraph_text = document.createTextNode("This product is very good");
+// append text to heading and paragraph
+my_product_heading.appendChild(my_heading_text);
+my_product_paragraph.appendChild(my_paragraph_text);
+// append heading and paragraph to product div
+my_product.appendChild(my_product_heading);
+my_product.appendChild(my_product_paragraph);
+
+my_product.className = "product";
+
+document.body.appendChild(my_product);
+
 
 
 

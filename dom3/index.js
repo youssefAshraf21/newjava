cef63365515dcdd0dd9e12760075eb56841439e1
youssefAshraf21 <ya614962@gmail.com>
2025-12-20 00:10:14 +0200

    Add initial HTML and JavaScript files for basic structure and element selection

---
+++ dom3/index.js
@@ -0,0 +1,9 @@
+let my_element = document.querySelector("div");
+
+console.log(my_element);
+console.log(my_element.children[0]);
+console.log(my_element.childNodes);
+console.log(my_element.firstChild);
+console.log(my_element.lastChild);
+console.log(my_element.firstElementChild);
+console.log(my_element.lastElementChild);

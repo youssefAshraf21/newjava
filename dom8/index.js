9d55f502afa53b6d3a8d506a58259620d6b7f701
youssefAshraf21 <ya614962@gmail.com>
2025-12-27 17:02:58 +0200

    Add initial HTML and JavaScript files for basic structure and DOM manipulation

---
+++ dom8/index.js
@@ -0,0 +1,9 @@
+let elm = document.getElementById("my_div");
+// console.log(elm);
+// let creatp = document.createElement("p");
+// creatp.innerText = "This is created using js";
+// elm.before("Hello World from js");
+// elm.after(creatp);
+elm.append("This is appended text");
+elm.prepend("This is prepended text ");
+// elm.remove();

9e469a2fc7121ce1dad6e1f1e0a8cfef0ca5c59a
youssefAshraf21 <ya614962@gmail.com>
2026-01-19 14:24:12 +0200

    Refactor user object destructuring for improved readability

--- destObj1/index.js
+++ destObj1/index.js
@@ -2,7 +2,13 @@ const user = {
     TheName : "Youssef",
     TheAge  : 25,
     TheGender : "Male",
-    TheCountry : "Egypt"      
+    TheCountry : "Egypt",  
+    TheColor : "red",
+    skills : {
+        html : 100,
+        css  : 90,
+        js   : 80
+    }
 }
 // let theName = user.TheName;
 // let theAge = user.TheAge;
@@ -10,9 +16,13 @@ const user = {
 // let theCountry = user.TheCountry;
 
 // ({TheName , TheAge , TheGender , TheCountry} = user); 
-const {TheName , TheAge , TheGender , TheCountry} = user;
+const {TheName: a , TheAge: b , TheGender: c , TheCountry: d , TheColor: e = "Black" , skills: {html: h, css: s, js: j}} = user;
 
-console.log(TheName); // Youssef
-console.log(TheAge);  // 25
-console.log(TheGender); // Male
-console.log(TheCountry); // Egypt
+console.log(a); // Youssef
+console.log(b);  // 25
+console.log(c); // Male
+console.log(d); // Egypt
+console.log(e); // Black
+console.log(` my html skill is ${h}`); // my html skill is 100
+console.log(` my css skill is ${s}`); // my css skill is 90
+console.log(` my js skill is ${j}`); // my js skill is 80
